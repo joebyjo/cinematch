4cc6043455acdca95e83b547f34804c3b3a9d5d7
joebyjo <joebyjo@gmail.com>
2025-04-16 05:30:46 +0000

    - updated index.js - added axios to package.json - added print URL to console on startup - added .editorconfig

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,8 +3,12 @@ const path = require('path');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render(path.join(__dirname, '../../frontend/index.html'));
+router.get('/', function (req, res, next) {
+    res.redirect('/home');
+});
+
+router.get('/home', function (req, res, next) {
+    res.sendFile(path.join(__dirname, '../../frontend/home.html'));
 });
 
 
