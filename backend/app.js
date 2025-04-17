78d2920affb1123b271b5a3fd911e5316aa76d64
joebyjo <joebyjo@gmail.com>
2025-04-17 11:37:36 +0000

    - added movies.js - updated app.js

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const moviesRouter = require('./routes/movies');
 
 const app = express();
 
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, '../frontend'), { index: false })); 
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/movies', moviesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
