f16fb15f6cf48945f04cbed289f782165b741901
joebyjo <joebyjo@gmail.com>
2025-04-18 10:17:13 +0000

    - finished /movies/:id

--- backend/services/tmdb.js
+++ backend/services/tmdb.js
@@ -1,11 +1,13 @@
 const axios = require('axios');
 require('dotenv').config();
 
-const { TMDB_API_KEY } = process.env;
+const { TMDB_API_KEY, OMDB_API_KEY } = process.env;
 const BASE_URL = 'https://api.themoviedb.org/3';
 const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p';
+const OMDB_URL = 'https://www.omdbapi.com/';
 
 
+// creating tmdb obj
 const tmdb = axios.create({
     baseURL: BASE_URL,
     headers: {
@@ -14,5 +16,23 @@ const tmdb = axios.create({
     }
 });
 
+// getting critic ratings from OMDB API
+async function getImdbData(movieId) {
+    try {
+        const response = await axios.get(`${OMDB_URL}?i=${movieId}&apikey=${OMDB_API_KEY}`);
+        const results = {
+            ratings: response.data.Ratings,
+            director: response.data.Director,
+            cast: response.data.Actors
+        };
 
-module.exports = tmdb;
+        return results;
+
+    } catch (error) {
+        console.error(`OMDB fetch failed: ${error.message}`);
+        return null;
+    }
+}
+
+
+module.exports = { tmdb, getImdbData };
