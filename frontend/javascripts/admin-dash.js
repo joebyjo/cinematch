3d581fead3bc56021678fb30dce09327ce687276
josheen <a1912141@adelaide.edu.au>
2025-06-09 09:42:38 +0000

    - made table section - added dummy data to js to display

---
+++ frontend/javascripts/admin-dash.js
@@ -0,0 +1,89 @@
+createApp({
+    data() {
+        // dummy data
+        return {
+            users: [
+                {
+                    username: 'josheen_1',
+                    firstname: 'Josheen',
+                    lastname: 'Kour',
+                    role: 'Administrator',
+                    dateJoined: '01/01/2025',
+                    pfp: 'avatar 1'
+                },
+                {
+                    username: 'liri_1',
+                    firstname: 'Subhashree',
+                    lastname: 'Das',
+                    role: 'User',
+                    dateJoined: '02/02/2025',
+                    pfp: 'uploaded image : link'
+                },
+                {
+                    username: 'joe_1',
+                    firstname: 'Joe Byjo',
+                    lastname: 'Puthussery',
+                    role: 'Administrator',
+                    dateJoined: '03/03/2025',
+                    pfp: 'avatar 2'
+                },
+                {
+                    username: 'hiten_1',
+                    firstname: 'Hiten',
+                    lastname: 'Gupta',
+                    role: 'User',
+                    dateJoined: '04/04/2025',
+                    pfp: 'avatar 5'
+                },
+                {
+                    username: 'josheen_2',
+                    firstname: 'Josheen',
+                    lastname: 'Kour',
+                    role: 'User',
+                    dateJoined: '05/01/2025',
+                    pfp: 'uploaded image : link'
+                },
+                {
+                    username: 'liri_2',
+                    firstname: 'Subhashree',
+                    lastname: 'Das',
+                    role: 'Administrator',
+                    dateJoined: '06/02/2025',
+                    pfp: 'uploaded image : link'
+                },
+                {
+                    username: 'joe_2',
+                    firstname: 'Joe Byjo',
+                    lastname: 'Puthussery',
+                    role: 'User',
+                    dateJoined: '07/03/2025',
+                    pfp: 'avatar 3'
+                },
+                {
+                    username: 'hiten_2',
+                    firstname: 'Hiten',
+                    lastname: 'Gupta',
+                    role: 'Administrator',
+                    dateJoined: '08/04/2025',
+                    pfp: 'uploaded image : link'
+                },
+                {
+                    username: 'josheen_3',
+                    firstname: 'Josheen',
+                    lastname: 'Kour',
+                    role: 'User',
+                    dateJoined: '09/01/2025',
+                    pfp: 'avatar 4'
+                },
+                {
+                    username: 'liri_3',
+                    firstname: 'Subhashree',
+                    lastname: 'Das',
+                    role: 'User',
+                    dateJoined: '10/02/2025',
+                    pfp: 'avatar 2'
+                }
+            ]
+        };
+    }
+}).mount('#admin-dash');
