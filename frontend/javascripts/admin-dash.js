f6409129d320393181d2d19ab9ec2f83448e70fa
josheen <a1912141@adelaide.edu.au>
2025-06-11 07:25:57 +0000

    - added a logout timer which logs admin out if inactive over 3 mins

--- frontend/javascripts/admin-dash.js
+++ frontend/javascripts/admin-dash.js
@@ -47,6 +47,8 @@ createApp({
                 special: false,
                 noSpaces: false
             },
+            logoutTime: null, // Timer for inactivity
+            inactiveTime: 180000, // 3 minutes in milliseconds
 
             // dummy data
             users: [
@@ -234,13 +236,15 @@ createApp({
         };
     },
     methods: {
+        // turn on select button
         toggleSelect() {
             this.isSelectOn = !this.isSelectOn;
             if (!this.isSelectOn) {
                 this.selectedUsers = [];
             }
         },
-        SelectUser(username) {
+        // select users
+        selectUser(username) {
             const index = this.selectedUsers.indexOf(username);
             if (index === -1) {
                 this.selectedUsers.push(username);
@@ -248,16 +252,20 @@ createApp({
                 this.selectedUsers.splice(index, 1);
             }
         },
+        // add a new user to the table
         addUser() {
+            // check if all fields are filled
             if (Object.values(this.newUser).some((val) => !val)) {
                 alert('Please fill all fields.');
                 return;
             }
+            // validate password
             this.validatePass(this.newUser.password);
             if (!this.isPassValid()) {
                 this.passError = 'Password did not meet requirements';
                 return;
             }
+            // push added data
             this.users.push({
                 username: this.newUser.username,
                 firstname: this.newUser.firstname,
@@ -267,6 +275,7 @@ createApp({
                 pfp: this.newUser.pfp,
                 lastActive: 'Not active'
             });
+            // reset add form once done
             this.resetForm();
         },
         resetForm() {
@@ -296,6 +305,7 @@ createApp({
                 fileInput.value = '';
             }
         },
+        // upload image
         imageUpload(event, isEditing) {
             const file = event.target.files[0];
             this.uploadError = '';
@@ -303,16 +313,19 @@ createApp({
             if (!file) {
                 return;
             }
+            // check if file type is valid
             if (!file.type.match('image/jpg') && !file.type.match('image/jpeg') && !file.type.match('image/png')) {
                 this.uploadError = 'Please upload a JPG/JPEG/PNG file';
                 return;
             }
+            // check if file size is valid
             if (file.size > 1024*1024) {
                 this.uploadError = 'Max file size allowed is 1MB';
                 return;
             }
             const reader = new FileReader();
             reader.onload = (e) => {
+                // check if upload is for editing user or adding new
                 if (isEditing) {
                     this.editingUser.pfpPreview = e.target.result;
                     this.editingUser.pfp = 'uploaded image';
@@ -323,7 +336,9 @@ createApp({
             };
             reader.readAsDataURL(file);
         },
+        // select pre made avatars
         selectAv(avatar, isEditing) {
+            // set avatar 3 as default if no av input entered
             if (!avatar) {
                 if (isEditing) {
                     this.editingUser.pfp = 'avatar3';
@@ -334,6 +349,7 @@ createApp({
                 }
                 return;
             }
+            // check if selection is for editing or adding new user
             if (isEditing) {
                 this.editingUser.pfp = avatar;
                 this.editingUser.pfpPreview = `./images/settings/${avatar}.svg`;
@@ -342,6 +358,7 @@ createApp({
                 this.newUser.pfpPreview = `./images/settings/${avatar}.svg`;
             }
         },
+        // edit an existing user
         editUser(user) {
             this.editingUser = {
                 username: user.username,
@@ -353,6 +370,7 @@ createApp({
             };
             this.showEditUser = true;
         },
+        // save edits made to existing user
         saveEdits() {
             const userIdx = this.users.findIndex((u) => u.username === this.editingUser.username);
             if (userIdx!== -1) {
@@ -366,6 +384,7 @@ createApp({
             }
             this.showEditUser = false;
         },
+        // check if password meets req while adding new user
         validatePass(password) {
             this.passReq = {
                 length: password.length >= 8 && password.length <= 16,
@@ -378,6 +397,19 @@ createApp({
         },
         isPassValid() {
             return Object.values(this.passReq).every((req) => req);
+        },
+        // logout user for being inactive over 3 mins
+        logoutUser() {
+            helperLogout('api/auth/logout');
+            window.location.href = '/home';
+            alert('You were logged out due to inactivity.');
+        },
+        // reset the timer
+        resetLogoutTime() {
+            clearTimeout(this.logoutTime);
+            this.logoutTime = setTimeout(() => {
+                this.logoutUser();
+            }, this.inactiveTime);
         }
     },
     computed: {
@@ -426,5 +458,8 @@ createApp({
                 this.showFilters = false;
             }
         });
+        // check for cicks to stay logged in
+        this.resetLogoutTime();
+        document.addEventListener('mousemove', this.resetLogoutTime);
     }
 }).mount('#admin-dash');
