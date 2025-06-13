a329241cc8206e39b852c86bfb5db28b147eb859
joebyjo <joebyjo@gmail.com>
2025-06-13 07:05:33 +0000

    - added better fallback - added SALT_ROUNDS to constants.js - fixed minor eslint errors

--- backend/services/constants.js
+++ backend/services/constants.js
@@ -1,7 +1,9 @@
+// salt rounds for hashing
+const SALT_ROUNDS = 10;
 
-
+// preffered watch_providers and countries for certifications
 const preferredProviders = [8, 9, 2, 10, 337, 531, 15, 350, 1968, 386, 1770, 1899];
 const preferredCountries = ['AU', 'US', 'GB', 'IN'];
 
 
-module.exports = [preferredProviders, preferredCountries];
+module.exports = [preferredProviders, preferredCountries, SALT_ROUNDS];
